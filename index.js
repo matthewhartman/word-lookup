eaaa31042110b23dbd08312d178c1095fb35a51e
Matthew Hartman <matthewhartmanau@gmail.com>
2022-04-19 22:25:01 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+'use strict';
+
+import chalk from 'chalk';
+import fetch from 'node-fetch';
+
+var args = process.argv.slice(2);
+
+const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${args[0]}`);
+const body = await response.json();
+
+const { word, phonetic, meanings } = body[0];
+const { definitions } = meanings[0];
+
+const white = chalk.hex('#fff');
+const smoke = chalk.hex('#ccc');
+const darksmoke = chalk.hex('#aaa');
+const grey = chalk.hex('#777');
+
+console.log(`\n${white.bold(word)} ${grey(phonetic)}\n`);
+console.log(darksmoke(definitions.map((word, index) => `${++index}. ${word.definition} ${word.example ? `${grey(`example: ${word.example}`)}` : ''}\n`).join('')));
---
+++ readme.markdown
@@ -0,0 +1,14 @@
+# Word Lookup
+
+Look up dictionary words straight in the terminal.
+
+## Installation
+
+Simply run `npm i -g word-lookup` to install define globally.
+
+Once installed you can run the following commands:
+
+```bash
+# look up word definition via dictionary
+define painting
+```
