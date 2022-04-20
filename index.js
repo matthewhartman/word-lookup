c6f0d2390e40c25e9e5c0bcfa0f384bc06fa960a
Matthew Hartman <matthewhartmanau@gmail.com>
2022-04-20 12:43:31 +1000

    Added some basic error handling

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ var args = process.argv.slice(2);
 const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${args[0]}`);
 const body = await response.json();
 
-const { word, phonetic, meanings } = body[0];
-const { definitions } = meanings[0];
+if (body[0]) {
+  const { word, phonetic, meanings } = body[0];
+  const { definitions } = meanings[0];
 
-const white = chalk.hex('#fff');
-const smoke = chalk.hex('#ccc');
-const darksmoke = chalk.hex('#aaa');
-const grey = chalk.hex('#777');
+  const white = chalk.hex('#fff');
+  const smoke = chalk.hex('#ccc');
+  const darksmoke = chalk.hex('#aaa');
+  const grey = chalk.hex('#777');
 
-console.log(`\n${white.bold(word)} ${grey(phonetic)}\n`);
-console.log(darksmoke(definitions.map((word, index) => `${++index}. ${word.definition} ${word.example ? `${grey(`example: ${word.example}`)}` : ''}\n`).join('')));
+  console.log(`\n${white.bold(word)} ${phonetic ? grey(phonetic) : ''}\n`);
+  console.log(darksmoke(definitions.map((word, index) => `${++index}. ${word.definition} ${word.example ? `${grey(`example: ${word.example}`)}` : ''}\n`).join('')));
+} else {
+  console.log(`Could not find definition for ${args[0]}`);
+}
